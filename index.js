604a4cfec8f7b50f43c4c33af0aaf16ca4c1d0a6
bonveeee <bonochieng@gmail.com>
2022-06-09 02:00:29 +0300

    commit tree 45b6543fbc163727d987941aab98c715ddece464

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+const leastFavoriteCustomer = "clegon"
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+  }
+
+function overwriteBestCustomer(){
+    return bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = "dido"
+}
